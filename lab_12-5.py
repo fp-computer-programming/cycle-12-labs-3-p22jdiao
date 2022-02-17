7675f2e9c4357acf95cdfe4d022c664b97cb303d
p22jdiao <p22jdiao@fairfieldprep.org>
2022-02-17 09:54:18 -0500

    finished

---
+++ lab_12-5.py
@@ -0,0 +1,5 @@
+# Author: JD 02/17/2022
+
+row = {"B202-08":"Declan", "B202-07":"Charlie", "B202-06": "Michael", "B202-05": "Mohamed", "B202-04": "Jordan", "B202-03": "Jason", "B202-02": "Mateo"}
+
+print(row)
